d330041286496c16497ae82ee24bf6268bec231d
zhongjibing <zhongjibing@163.com>
2022-12-27 17:13:30 +0800

    rename

--- src/main/java/com/icezhg/commons/util/IdGenerator.java
+++ src/main/java/com/icezhg/commons/util/IdGenerator.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicLong;
 /**
  * Created by zhongjibing on 2022/12/26.
  */
-public final class IdGenerate {
+public final class IdGenerator {
 
     private static final char[] DICT = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
             'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'};
@@ -85,12 +85,5 @@ public final class IdGenerate {
         final char[] buf = new char[repeat];
         Arrays.fill(buf, '0');
         return new String(buf);
-    }
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-
-            System.out.println(nextId());
-        }
     }
 }
