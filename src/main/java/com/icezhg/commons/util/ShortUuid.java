28abd8d75250a3095897bc37a40d6a5c8f75726f
zhongjibing <zhongjibing@163.com>
2023-08-04 04:06:04 +0800

    add ShortUuid

---
+++ src/main/java/com/icezhg/commons/util/ShortUuid.java
@@ -0,0 +1,27 @@
+package com.icezhg.commons.util;
+
+import java.util.UUID;
+
+/**
+ * Created by zhongjibing on 2019/01/06.
+ */
+public final class ShortUuid {
+    private static final char[] CHARS = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    private ShortUuid() {
+    }
+
+
+    private static String random() {
+        StringBuilder shortBuffer = new StringBuilder();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        for (int i = 0; i < 8; i++) {
+            String str = uuid.substring(i * 4, i * 4 + 4);
+            int x = Integer.parseInt(str, 16);
+            shortBuffer.append(CHARS[x % 0x3E]);
+        }
+        return shortBuffer.toString();
+    }
+
+
+}
