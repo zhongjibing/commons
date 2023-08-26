62735246c859b41a7737ee83990220984c5c18db
zhongjibing <zhongjibing@163.com>
2023-08-26 21:50:42 +0800

    MemCache opt

--- src/main/java/com/icezhg/commons/cache/MemCache.java
+++ src/main/java/com/icezhg/commons/cache/MemCache.java
@@ -6,14 +6,19 @@ import java.util.function.Supplier;
  * Created by zhongjibing on 2019/08/06
  */
 public final class MemCache<D> {
+    private static final long DEFAULT_EXPIRE_TIME = 60 * 1000L;
     private final Supplier<D> supplier;
     private final long expiretime;
     private volatile long timestamp;
     private volatile D data;
 
     public MemCache(Supplier<D> supplier) {
+        this(supplier, DEFAULT_EXPIRE_TIME);
+    }
+
+    public MemCache(Supplier<D> supplier, long expireMills) {
         this.supplier = supplier;
-        this.expiretime = 60 * 1000L;
+        this.expiretime = expireMills;
     }
 
     public D getData() {
